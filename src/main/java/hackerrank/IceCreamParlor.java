722933a582b86143c0a015665712eb34446b3e2f
adrian <adrian.francisco@canada.ca>
2017-01-30 15:18:33 -0500

    testing author and commiter changes

--- src/main/java/hackerrank/IceCreamParlor.java
+++ src/main/java/hackerrank/IceCreamParlor.java
@@ -11,6 +11,6 @@ public class IceCreamParlor {
      * @param args the arguments
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println("Hello World");
     }
 }
