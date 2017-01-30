d84f7914925956775b1e5649985bd1765cb01ea8
adrian <adrian@grimlock>
2017-01-30 13:40:48 -0500

    just some minor cleanups

--- src/main/java/playground/PlayGround.java
+++ src/main/java/playground/PlayGround.java
@@ -1,10 +1,5 @@
 package playground;
 
-import java.io.File;
-
-import ca.gc.ec.dms.commons.metadata.MDInstance;
-import ca.gc.ec.dms.commons.util.XMLPrint;
-
 /**
  * My Play Ground.
  */
@@ -17,11 +12,6 @@ public class PlayGround {
      * @throws Exception on any exception
      */
     public static void main(String[] args) throws Exception {
-        MDInstance instance = new MDInstance();
-        instance.parse(new File("src/test/resources/element_11_1001.xml"));
 
-        System.out.println(XMLPrint.prettyPrint(new String(instance.generateXMLAsByte())));
-
-        System.out.println(instance.getCreatedBy());
     }
 }
