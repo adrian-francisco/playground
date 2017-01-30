4ee29b07c2c41bbc5808763385a1a4e1bb2dcfa6
adrian <adrian@grimlock>
2017-01-30 13:54:48 -0500

    added day 03 and 17

--- src/main/java/hackerrank/Day03.java
+++ src/main/java/hackerrank/Day03.java
@@ -2,7 +2,7 @@ package hackerrank;
 
 import java.util.Scanner;
 
-public class Solution {
+public class Day03 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
