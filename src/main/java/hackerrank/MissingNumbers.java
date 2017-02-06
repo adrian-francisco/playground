59c7906b90d8c56fcb29238be8aa6ed63df94090
adrian.francisco <adrian.francisco@canada.ca>
2017-02-06 15:12:20 +0000

    fixed the missing numbers challenge

--- src/main/java/hackerrank/MissingNumbers.java
+++ src/main/java/hackerrank/MissingNumbers.java
@@ -4,73 +4,90 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MissingNumbers {
 
-	public static void main(String... args) {
-		Scanner in = new Scanner(System.in);
+    public static void main(String... args) {
+        Scanner in = new Scanner(System.in);
 
-		int n = in.nextInt();
-		List<Integer> a = new ArrayList<>();
+        Map<Integer, Integer> map = new TreeMap<>();
 
-		for (int i = 0; i < n; i++) {
-			a.add(in.nextInt());
-		}
+        int n = in.nextInt();
 
-		int m = in.nextInt();
-		Set<Integer> b = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            Integer v = map.get(x);
 
-		for (int i = 0; i < m; i++) {
-			int x = in.nextInt();
+            if (v == null) {
+                map.put(x, -1);
+            }
+            else {
+                map.put(x, v - 1);
+            }
+        }
 
-			if (!a.remove((Object) x)) {
-				b.add(x);
-			}
-		}
+        int m = in.nextInt();
 
-		in.close();
+        for (int i = 0; i < m; i++) {
+            int x = in.nextInt();
+            Integer v = map.get(x);
 
-		for (Integer i : b) {
-			System.out.print(i + " ");
-		}
-	}
+            if (v == null) {
+                map.put(x, 1);
+            }
+            else {
+                map.put(x, v + 1);
+            }
+        }
 
-	@Test
-	public void testInput0() throws Exception {
-		StringBuilder input = new StringBuilder();
-		input.append("10\n");
-		input.append("203 204 205 206 207 208 203 204 205 206\n");
-		input.append("13\n");
-		input.append("203 204 204 205 206 207 205 208 203 206 205 206 204\n");
+        in.close();
 
-		System.setIn(new ByteArrayInputStream(input.toString().getBytes("UTF-8")));
+        for (Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(baos));
+            if (value > 0) {
+                System.out.print(key + " ");
+            }
+        }
 
-		MissingNumbers.main();
+    }
 
-		Assert.assertEquals("204 205 206 ", baos.toString());
-	}
+    @Test
+    public void testInput0() throws Exception {
+        StringBuilder input = new StringBuilder();
+        input.append("10\n");
+        input.append("203 204 205 206 207 208 203 204 205 206\n");
+        input.append("13\n");
+        input.append("203 204 204 205 206 207 205 208 203 206 205 206 204\n");
 
-	@Test
-	public void testInput1() throws Exception {
-		FileInputStream fis = new FileInputStream("src/main/resources/hackerrank/MissingNumbersInput04.txt");
-		System.setIn(fis);
+        System.setIn(new ByteArrayInputStream(input.toString().getBytes("UTF-8")));
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(baos));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
 
-		MissingNumbers.main();
+        MissingNumbers.main();
 
-		Assert.assertEquals("7251 7259 7276 7279 7292 7293 ", baos.toString());
-	}
+        Assert.assertEquals("204 205 206 ", baos.toString());
+    }
+
+    @Test
+    public void testInput1() throws Exception {
+        FileInputStream fis = new FileInputStream("src/main/resources/hackerrank/MissingNumbersInput04.txt");
+        System.setIn(fis);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        MissingNumbers.main();
+
+        Assert.assertEquals("7251 7259 7276 7279 7292 7293 ", baos.toString());
+    }
 }
