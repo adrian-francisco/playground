54e23d168e1da5faf18597e4e0fda46284355009
adrian.francisco <adrian.francisco@canada.ca>
2017-02-10 15:16:16 +0000

    adding statistics utility

---
+++ src/main/java/hackerrank/Statistics.java
@@ -0,0 +1,174 @@
+/*
+ * Name : Statistics.java
+ * Author : Adrian Francisco
+ * Created: Feb 10, 2017
+ */
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Utility methods for various statistics calculations.
+ *
+ * @author Adrian Francisco
+ */
+public class Statistics {
+
+    /*
+     **************************************** CONSTRUCTORS *************************************************************
+     */
+
+    /**
+     * Creates a new instance of Statistics.
+     */
+    private Statistics() {
+    }
+
+    /*
+     **************************************** PUBLIC METHODS ***********************************************************
+     */
+
+    /**
+     * Calculate the mean of the array.
+     *
+     * @param array the array
+     * @return the mean
+     */
+    public static double mean(double[] array) {
+        int n = array.length;
+
+        double mean = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            mean += array[i];
+        }
+
+        mean = mean / n;
+
+        return mean;
+    }
+
+    /**
+     * Calculate the median of the array.
+     *
+     * @param array the array
+     * @return the median
+     */
+    public static double median(double[] array) {
+        int n = array.length;
+        int half = n / 2;
+        boolean even = n % 2 == 0;
+
+        Arrays.sort(array);
+
+        double median;
+
+        if (even) {
+            median = (array[half - 1] + array[half]) / 2.0;
+        }
+        else {
+            median = array[half];
+        }
+
+        return median;
+    }
+
+    /**
+     * Calculate the mode of the array.
+     *
+     * @param array the array
+     * @return the mode
+     */
+    public static double mode(double[] array) {
+        int n = array.length;
+
+        Map<Double, Integer> occurances = new TreeMap<>();
+
+        int mode = 0;
+
+        for (int i = 0; i < n; i++) {
+            double k = array[i];
+
+            if (!occurances.containsKey(k)) {
+                occurances.put(k, 0);
+            }
+
+            int newMode = occurances.get(k) + 1;
+
+            if (newMode > mode) {
+                mode = newMode;
+            }
+
+            occurances.put(k, newMode);
+        }
+
+        for (Map.Entry<Double, Integer> entry : occurances.entrySet()) {
+            if (entry.getValue() == mode) {
+                return entry.getKey();
+            }
+        }
+
+        return Double.NaN;
+    }
+
+    /**
+     * Calculate the weighted mean of the given array and weight.
+     *
+     * @param array the array
+     * @param weight the weight
+     * @return the weighted mean
+     */
+    public static double weightedMean(double[] array, double[] weight) {
+        if (array.length != weight.length) {
+            return Double.NaN;
+        }
+
+        int n = array.length;
+
+        double top = 0.0;
+        double bottom = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            top += array[i] * weight[i];
+            bottom += weight[i];
+        }
+
+        return top / bottom;
+    }
+
+    /**
+     * Calculate the quartiles of the given array. The returned array will contain 3 doubles: q1, q2, and q3,
+     * respectively.
+     *
+     * @param array the array
+     * @return the quartiles
+     */
+    public static double[] quartiles(double[] array) {
+        int n = array.length;
+        int half = n / 2;
+        boolean even = n % 2 == 0;
+
+        Arrays.sort(array);
+
+        double[] quartiles = new double[] {Double.NaN, Double.NaN, Double.NaN};
+
+        quartiles[1] = median(array);
+
+        if (even) {
+            quartiles[0] = median(Arrays.copyOfRange(array, 0, half));
+            quartiles[2] = median(Arrays.copyOfRange(array, half, n));
+        }
+        else {
+            quartiles[0] = median(Arrays.copyOfRange(array, 0, half));
+            quartiles[2] = median(Arrays.copyOfRange(array, half + 1, n));
+        }
+
+        return quartiles;
+    }
+
+    /*
+     **************************************** PRIVATE METHODS **********************************************************
+     */
+}
---
+++ src/main/java/hackerrank/StatisticsTest.java
@@ -0,0 +1,147 @@
+/*
+ * Name : StatisticsTest.java
+ * Author : Adrian Francisco
+ * Created: Feb 10, 2017
+ */
+package hackerrank;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Unit tests for the Statistics class.
+ *
+ * @author Adrian Francisco
+ */
+public class StatisticsTest {
+
+    /*
+     **************************************** PUBLIC FIELDS ************************************************************
+     */
+
+    /*
+     **************************************** PRIVATE FIELDS ***********************************************************
+     */
+
+    /** the logging. */
+    private static final Log LOG = LogFactory.getLog(StatisticsTest.class);
+
+    /*
+     **************************************** CONSTRUCTORS *************************************************************
+     */
+
+    /**
+     * Creates a new instance of StatisticsTest.
+     */
+    public StatisticsTest() {
+    }
+
+    /*
+     **************************************** FIXTURES *****************************************************************
+     */
+
+    /**
+     * Sets the up before class.
+     *
+     * @throws Exception the exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * Tear down after class.
+     *
+     * @throws Exception the exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * Sets the up.
+     *
+     * @throws Exception the exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Tear down.
+     *
+     * @throws Exception the exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /*
+     **************************************** TEST METHODS *************************************************************
+     */
+
+    /**
+     * Test mean.
+     */
+    @Test
+    public final void testMean() {
+        assertEquals(43900.6,
+                Statistics.mean(new double[] {64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060}), 0);
+    }
+
+    /**
+     * Test median.
+     */
+    @Test
+    public final void testMedian() {
+        assertEquals(2.5, Statistics.median(new double[] {1, 2, 3, 4}), 0);
+        assertEquals(2, Statistics.median(new double[] {1, 2, 3}), 0);
+    }
+
+    /**
+     * Test mode.
+     */
+    @Test
+    public final void testMode() {
+        assertEquals(4978,
+                Statistics.mode(new double[] {64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060}), 0);
+    }
+
+    /**
+     * Test weighted mean.
+     */
+    @Test
+    public final void testWeightedMean() {
+        assertEquals(32, Statistics.weightedMean(new double[] {10, 40, 30, 50, 20}, new double[] {1, 2, 3, 4, 5}), 0);
+    }
+
+    /**
+     * Test quartiles.
+     */
+    @Test
+    public void testQuartiles() {
+        double[] q = null;
+
+        q = Statistics.quartiles(new double[] {3, 7, 8, 5, 12, 14, 21, 13, 18});
+        assertEquals(3, q.length);
+        assertEquals(6, q[0], 0);
+        assertEquals(12, q[1], 0);
+        assertEquals(16, q[2], 0);
+
+        q = Statistics.quartiles(new double[] {3, 7, 8, 5, 12, 14, 21, 15, 18, 14});
+        assertEquals(3, q.length);
+        assertEquals(7, q[0], 0);
+        assertEquals(13, q[1], 0);
+        assertEquals(15, q[2], 0);
+    }
+
+    /*
+     **************************************** PRIVATE METHODS **********************************************************
+     */}
