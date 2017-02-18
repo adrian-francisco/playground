ea1f09751b351cdcd3a30ad50bf338ab34283080
adrian.francisco <adrian.francisco@canada.ca>
2017-02-18 03:58:05 +0000

    solved davis staircase

--- src/main/java/hackerrank/DavisStaircase.java
+++ src/main/java/hackerrank/DavisStaircase.java
@@ -5,81 +5,49 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.junit.Test;
 
 public class DavisStaircase {
 
+    private static Map<Integer, Long> cache = new HashMap<>();
+
     public static void main(String... args) {
         Scanner in = new Scanner(System.in);
         int s = in.nextInt();
 
-        int[] steps = new int[] {1, 2, 3};
-        int m = steps.length;
+        cache.put(1, 1L);
+        cache.put(2, 2L);
+        cache.put(3, 4L);
 
-        for (int a0 = 0; a0 < s; a0++) {
+        for (int i = 0; i < s; i++) {
             int n = in.nextInt();
-
-            long[][] cache = new long[m + 1][n + 1];
-            for (int i = 0; i <= m; i++) {
-                for (int j = 0; j <= n; j++) {
-                    cache[i][j] = 0;
-                }
-            }
-
-            System.out.println(steps(steps, m, n, cache));
-
-            for (int i = 0; i <= m; i++) {
-                for (int j = 0; j <= n; j++) {
-                    System.out.print(cache[i][j] + " ");
-                }
-                System.out.println();
-            }
+            System.out.println(steps(n));
         }
 
         in.close();
     }
 
-    public static long steps(int[] steps, int m, int n, long[][] cache) {
-        if (n == 0) {
-            return 1;
+    public static long steps(int n) {
+        if (cache.containsKey(n)) {
+            return cache.get(n);
         }
 
-        if (n < 0) {
-            return 0;
-        }
-
-        if (m <= 0) {
-            return 0;
-        }
-
-        if (cache[m][n] > 0) {
-            return cache[m][n];
-        }
-
-        long i = steps(steps, m - 1, n, cache);
-        long j = steps(steps, m, n - steps[m - 1], cache);
-
-        if (m - 1 >= 0) {
-            cache[m - 1][n] = i;
-        }
-
-        if (n - steps[m - 1] >= 0) {
-            cache[m][n - steps[m - 1]] = j;
-        }
-
-        cache[m][n] = i + j;
-        return cache[m][n];
+        cache.put(n, steps(n - 1) + steps(n - 2) + steps(n - 3));
+        return cache.get(n);
     }
 
     @Test
     public void test0() throws Exception {
         StringBuilder input = new StringBuilder();
-        input.append("3\n");
+        input.append("4\n");
         input.append("1\n");
         input.append("3\n");
         input.append("7\n");
+        input.append("36\n");
 
         System.setIn(new ByteArrayInputStream(input.toString().getBytes("UTF-8")));
 
@@ -88,6 +56,6 @@ public class DavisStaircase {
 
         main();
 
-        assertEquals("1\n4\n44", baos.toString().trim());
+        assertEquals("1\n4\n44\n2082876103", baos.toString().trim());
     }
 }
