1ef6953082c62e62353bae2c0255b5dd59b68d51
adrian.francisco <adrian.francisco@gmail.com>
2017-03-27 20:20:37 -0400

    incomplete java visitor pattern

---
+++ src/main/java/hackerrank/JavaVisitorPattern.java
@@ -0,0 +1,248 @@
+package hackerrank;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.junit.Assert;
+
+public class JavaVisitorPattern {
+
+	enum Color {
+		RED, GREEN
+	}
+
+	abstract class Tree {
+
+		private int value;
+		private Color color;
+		private int depth;
+
+		public Tree(int value, Color color, int depth) {
+			this.value = value;
+			this.color = color;
+			this.depth = depth;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public Color getColor() {
+			return color;
+		}
+
+		public int getDepth() {
+			return depth;
+		}
+
+		public abstract void accept(TreeVis visitor);
+	}
+
+	class TreeNode extends Tree {
+
+		private ArrayList<Tree> children = new ArrayList<>();
+
+		public TreeNode(int value, Color color, int depth) {
+			super(value, color, depth);
+		}
+
+		@Override
+		public void accept(TreeVis visitor) {
+			visitor.visitNode(this);
+
+			for (Tree child : children) {
+				child.accept(visitor);
+			}
+		}
+
+		public void addChild(Tree child) {
+			children.add(child);
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder("node: ");
+			sb.append(getValue()).append(" ");
+			sb.append(getColor()).append(" ");
+			sb.append(getDepth()).append(" -> ");
+			for (Tree child : children) {
+				sb.append(child.getValue()).append(" ");
+			}
+			sb.append("\n");
+			return sb.toString();
+		}
+	}
+
+	class TreeLeaf extends Tree {
+
+		public TreeLeaf(int value, Color color, int depth) {
+			super(value, color, depth);
+		}
+
+		@Override
+		public void accept(TreeVis visitor) {
+			visitor.visitLeaf(this);
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder("leaf: ");
+			sb.append(getValue()).append(" ");
+			sb.append(getColor()).append(" ");
+			sb.append(getDepth()).append("\n");
+			return sb.toString();
+		}
+	}
+
+	abstract class TreeVis {
+		public abstract int getResult();
+
+		public abstract void visitNode(TreeNode node);
+
+		public abstract void visitLeaf(TreeLeaf leaf);
+
+	}
+
+	class SumInLeavesVisitor extends TreeVis {
+		@Override
+		public int getResult() {
+			// implement this
+			return 0;
+		}
+
+		@Override
+		public void visitNode(TreeNode node) {
+			// implement this
+		}
+
+		@Override
+		public void visitLeaf(TreeLeaf leaf) {
+			// implement this
+		}
+	}
+
+	class ProductOfRedNodesVisitor extends TreeVis {
+		@Override
+		public int getResult() {
+			// implement this
+			return 1;
+		}
+
+		@Override
+		public void visitNode(TreeNode node) {
+			// implement this
+		}
+
+		@Override
+		public void visitLeaf(TreeLeaf leaf) {
+			// implement this
+		}
+	}
+
+	class FancyVisitor extends TreeVis {
+		@Override
+		public int getResult() {
+			// implement this
+			return 0;
+		}
+
+		@Override
+		public void visitNode(TreeNode node) {
+			// implement this
+		}
+
+		@Override
+		public void visitLeaf(TreeLeaf leaf) {
+			// implement this
+		}
+	}
+
+	private Tree[] trees = null;
+
+	public Tree solve() {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+
+		trees = new Tree[n];
+		int[] values = new int[n];
+		Color[] colors = new Color[n];
+		int[][] edges = new int[n - 1][3];
+
+		for (int i = 0; i < n; i++) {
+			values[i] = in.nextInt();
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (in.nextInt() == 0) {
+				colors[i] = Color.RED;
+			} else {
+				colors[i] = Color.GREEN;
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			trees[i] = new TreeLeaf(values[i], colors[i], 0);
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			int u = in.nextInt() - 1;
+			int v = in.nextInt() - 1;
+
+		}
+
+		in.close();
+		// if (trees[u] instanceof TreeLeaf) {
+		// TreeLeaf leaf = (TreeLeaf) trees[u];
+		// TreeNode node = new TreeNode(leaf.getValue(), leaf.getColor(), 0);
+		// node.addChild(trees[v]);
+		// trees[u] = node;
+		// } else {
+		// TreeNode node = (TreeNode) trees[u];
+		// node.addChild(trees[v]);
+		// }
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(i + ": " + trees[i]);
+		}
+
+		return trees[0];
+	}
+
+	public void main() {
+		Tree root = new JavaVisitorPattern().solve();
+		SumInLeavesVisitor vis1 = new SumInLeavesVisitor();
+		ProductOfRedNodesVisitor vis2 = new ProductOfRedNodesVisitor();
+		FancyVisitor vis3 = new FancyVisitor();
+
+		root.accept(vis1);
+		root.accept(vis2);
+		root.accept(vis3);
+
+		int res1 = vis1.getResult();
+		int res2 = vis2.getResult();
+		int res3 = vis3.getResult();
+
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
+	}
+
+	// @Test
+	public void test1() throws Exception {
+		String input = "5\n4 7 2 5 12\n0 1 0 0 1\n1 2\n1 3\n3 4\n3 5";
+
+		System.setIn(new ByteArrayInputStream(input.getBytes("UTF-8")));
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		// System.setOut(new PrintStream(baos, false, "UTF-8"));
+
+		main();
+
+		String output = "24 40 15";
+
+		Assert.assertEquals(output.trim(), baos.toString("UTF-8").trim());
+	}
+}
