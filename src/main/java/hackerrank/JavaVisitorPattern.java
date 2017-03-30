583c795f0685f5f9f9829e0753173dc940183462
adrian.francisco <adrian.francisco@canada.ca>
2017-03-30 18:06:53 +0000

    completed java visitor pattern

--- src/main/java/hackerrank/JavaVisitorPattern.java
+++ src/main/java/hackerrank/JavaVisitorPattern.java
@@ -2,247 +2,300 @@ package hackerrank;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class JavaVisitorPattern {
 
-	enum Color {
-		RED, GREEN
-	}
+    enum Color {
+        RED, GREEN
+    }
 
-	abstract class Tree {
+    abstract class Tree {
 
-		private int value;
-		private Color color;
-		private int depth;
+        private int value;
 
-		public Tree(int value, Color color, int depth) {
-			this.value = value;
-			this.color = color;
-			this.depth = depth;
-		}
+        private Color color;
 
-		public int getValue() {
-			return value;
-		}
+        private int depth;
 
-		public Color getColor() {
-			return color;
-		}
+        public Tree(int value, Color color, int depth) {
+            this.value = value;
+            this.color = color;
+            this.depth = depth;
+        }
 
-		public int getDepth() {
-			return depth;
-		}
+        public int getValue() {
+            return value;
+        }
 
-		public abstract void accept(TreeVis visitor);
-	}
+        public Color getColor() {
+            return color;
+        }
 
-	class TreeNode extends Tree {
+        public int getDepth() {
+            return depth;
+        }
 
-		private ArrayList<Tree> children = new ArrayList<>();
+        public abstract void accept(TreeVis visitor);
+    }
 
-		public TreeNode(int value, Color color, int depth) {
-			super(value, color, depth);
-		}
+    class TreeNode extends Tree {
 
-		@Override
-		public void accept(TreeVis visitor) {
-			visitor.visitNode(this);
+        private ArrayList<Tree> children = new ArrayList<>();
 
-			for (Tree child : children) {
-				child.accept(visitor);
-			}
-		}
+        public TreeNode(int value, Color color, int depth) {
+            super(value, color, depth);
+        }
 
-		public void addChild(Tree child) {
-			children.add(child);
-		}
+        @Override
+        public void accept(TreeVis visitor) {
+            visitor.visitNode(this);
 
-		@Override
-		public String toString() {
-			StringBuilder sb = new StringBuilder("node: ");
-			sb.append(getValue()).append(" ");
-			sb.append(getColor()).append(" ");
-			sb.append(getDepth()).append(" -> ");
-			for (Tree child : children) {
-				sb.append(child.getValue()).append(" ");
-			}
-			sb.append("\n");
-			return sb.toString();
-		}
-	}
+            for (Tree child : children) {
+                child.accept(visitor);
+            }
+        }
 
-	class TreeLeaf extends Tree {
+        public void addChild(Tree child) {
+            children.add(child);
+        }
 
-		public TreeLeaf(int value, Color color, int depth) {
-			super(value, color, depth);
-		}
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder("node ");
+            sb.append(getValue()).append(" ");
+            sb.append(getColor()).append(" ");
+            sb.append(getDepth()).append(" -> ");
+            for (Tree child : children) {
+                sb.append(child.getValue()).append(" ");
+            }
+            return sb.toString();
+        }
+    }
 
-		@Override
-		public void accept(TreeVis visitor) {
-			visitor.visitLeaf(this);
-		}
+    class TreeLeaf extends Tree {
 
-		@Override
-		public String toString() {
-			StringBuilder sb = new StringBuilder("leaf: ");
-			sb.append(getValue()).append(" ");
-			sb.append(getColor()).append(" ");
-			sb.append(getDepth()).append("\n");
-			return sb.toString();
-		}
-	}
+        public TreeLeaf(int value, Color color, int depth) {
+            super(value, color, depth);
+        }
 
-	abstract class TreeVis {
-		public abstract int getResult();
+        @Override
+        public void accept(TreeVis visitor) {
+            visitor.visitLeaf(this);
+        }
 
-		public abstract void visitNode(TreeNode node);
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder("leaf ");
+            sb.append(getValue()).append(" ");
+            sb.append(getColor()).append(" ");
+            sb.append(getDepth());
+            return sb.toString();
+        }
+    }
 
-		public abstract void visitLeaf(TreeLeaf leaf);
+    abstract class TreeVis {
 
-	}
+        public abstract int getResult();
 
-	class SumInLeavesVisitor extends TreeVis {
-		@Override
-		public int getResult() {
-			// implement this
-			return 0;
-		}
+        public abstract void visitNode(TreeNode node);
 
-		@Override
-		public void visitNode(TreeNode node) {
-			// implement this
-		}
+        public abstract void visitLeaf(TreeLeaf leaf);
 
-		@Override
-		public void visitLeaf(TreeLeaf leaf) {
-			// implement this
-		}
-	}
+    }
 
-	class ProductOfRedNodesVisitor extends TreeVis {
-		@Override
-		public int getResult() {
-			// implement this
-			return 1;
-		}
+    class SumInLeavesVisitor extends TreeVis {
 
-		@Override
-		public void visitNode(TreeNode node) {
-			// implement this
-		}
+        private int result = 0;
 
-		@Override
-		public void visitLeaf(TreeLeaf leaf) {
-			// implement this
-		}
-	}
+        @Override
+        public int getResult() {
+            return result;
+        }
 
-	class FancyVisitor extends TreeVis {
-		@Override
-		public int getResult() {
-			// implement this
-			return 0;
-		}
+        @Override
+        public void visitNode(TreeNode node) {
+        }
 
-		@Override
-		public void visitNode(TreeNode node) {
-			// implement this
-		}
+        @Override
+        public void visitLeaf(TreeLeaf leaf) {
+            result += leaf.getValue();
+        }
+    }
 
-		@Override
-		public void visitLeaf(TreeLeaf leaf) {
-			// implement this
-		}
-	}
+    class ProductOfRedNodesVisitor extends TreeVis {
 
-	private Tree[] trees = null;
+        private long result = 1;
 
-	public Tree solve() {
-		Scanner in = new Scanner(System.in);
+        private int modulo = (int) (Math.pow(10, 9) + 7);
 
-		int n = in.nextInt();
+        @Override
+        public int getResult() {
+            return (int) result;
+        }
 
-		trees = new Tree[n];
-		int[] values = new int[n];
-		Color[] colors = new Color[n];
-		int[][] edges = new int[n - 1][3];
+        @Override
+        public void visitNode(TreeNode node) {
+            if (node.getColor().equals(Color.RED)) {
+                result = result * node.getValue() % modulo;
+            }
+        }
 
-		for (int i = 0; i < n; i++) {
-			values[i] = in.nextInt();
-		}
+        @Override
+        public void visitLeaf(TreeLeaf leaf) {
+            if (leaf.getColor().equals(Color.RED)) {
+                result = result * leaf.getValue() % modulo;
+            }
+        }
+    }
 
-		for (int i = 0; i < n; i++) {
-			if (in.nextInt() == 0) {
-				colors[i] = Color.RED;
-			} else {
-				colors[i] = Color.GREEN;
-			}
-		}
+    class FancyVisitor extends TreeVis {
 
-		for (int i = 0; i < n; i++) {
-			trees[i] = new TreeLeaf(values[i], colors[i], 0);
-		}
+        private int evenResult = 0;
 
-		for (int i = 0; i < n - 1; i++) {
-			int u = in.nextInt() - 1;
-			int v = in.nextInt() - 1;
+        private int greenResult = 0;
 
-		}
+        @Override
+        public int getResult() {
+            return Math.abs(evenResult - greenResult);
+        }
 
-		in.close();
-		// if (trees[u] instanceof TreeLeaf) {
-		// TreeLeaf leaf = (TreeLeaf) trees[u];
-		// TreeNode node = new TreeNode(leaf.getValue(), leaf.getColor(), 0);
-		// node.addChild(trees[v]);
-		// trees[u] = node;
-		// } else {
-		// TreeNode node = (TreeNode) trees[u];
-		// node.addChild(trees[v]);
-		// }
+        @Override
+        public void visitNode(TreeNode node) {
+            if (node.getDepth() % 2 == 0) {
+                evenResult += node.getValue();
+            }
+        }
 
-		for (int i = 0; i < n; i++) {
-			System.out.println(i + ": " + trees[i]);
-		}
+        @Override
+        public void visitLeaf(TreeLeaf leaf) {
+            if (leaf.getColor().equals(Color.GREEN)) {
+                greenResult += leaf.getValue();
+            }
+        }
+    }
 
-		return trees[0];
-	}
+    public Tree solve() {
+        Scanner in = new Scanner(System.in);
 
-	public void main() {
-		Tree root = new JavaVisitorPattern().solve();
-		SumInLeavesVisitor vis1 = new SumInLeavesVisitor();
-		ProductOfRedNodesVisitor vis2 = new ProductOfRedNodesVisitor();
-		FancyVisitor vis3 = new FancyVisitor();
+        int n = in.nextInt();
 
-		root.accept(vis1);
-		root.accept(vis2);
-		root.accept(vis3);
+        Tree[] trees = new Tree[n];
+        int[] values = new int[n];
+        Color[] colors = new Color[n];
+        int[][] edges = new int[n - 1][2];
+        int[] depths = new int[n];
+        HashMap<Integer, ArrayList<Integer>> children = new HashMap<>();
 
-		int res1 = vis1.getResult();
-		int res2 = vis2.getResult();
-		int res3 = vis3.getResult();
+        for (int i = 0; i < n; i++) {
+            values[i] = in.nextInt();
+            children.put(i, new ArrayList<Integer>());
+        }
 
-		System.out.println(res1);
-		System.out.println(res2);
-		System.out.println(res3);
-	}
+        for (int i = 0; i < n; i++) {
+            if (in.nextInt() == 0) {
+                colors[i] = Color.RED;
+            }
+            else {
+                colors[i] = Color.GREEN;
+            }
+        }
 
-	// @Test
-	public void test1() throws Exception {
-		String input = "5\n4 7 2 5 12\n0 1 0 0 1\n1 2\n1 3\n3 4\n3 5";
+        for (int i = 0; i < n - 1; i++) {
+            int u = in.nextInt() - 1;
+            int v = in.nextInt() - 1;
+            edges[i] = new int[] {u, v};
+        }
 
-		System.setIn(new ByteArrayInputStream(input.getBytes("UTF-8")));
+        in.close();
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		// System.setOut(new PrintStream(baos, false, "UTF-8"));
+        LinkedList<Integer> nextNode = new LinkedList<>();
+        nextNode.offer(0);
+        depths[0] = 0;
 
-		main();
+        while (!nextNode.isEmpty()) {
+            int curNode = nextNode.poll();
 
-		String output = "24 40 15";
+            for (int i = 0; i < n - 1; i++) {
+                if (edges[i][0] == curNode) {
+                    int child = edges[i][1];
+                    nextNode.offer(child);
+                    depths[child] = depths[curNode] + 1;
+                    children.get(curNode).add(child);
+                    edges[i] = new int[] {-1, -1};
+                }
+                else if (edges[i][1] == curNode) {
+                    int child = edges[i][0];
+                    nextNode.offer(child);
+                    depths[child] = depths[curNode] + 1;
+                    children.get(curNode).add(child);
+                    edges[i] = new int[] {-1, -1};
+                }
+            }
+        }
 
-		Assert.assertEquals(output.trim(), baos.toString("UTF-8").trim());
-	}
+        // create the leaves and nodes
+        for (int i = 0; i < n; i++) {
+            if (children.get(i).isEmpty()) {
+                trees[i] = new TreeLeaf(values[i], colors[i], depths[i]);
+            }
+            else {
+                trees[i] = new TreeNode(values[i], colors[i], depths[i]);
+            }
+        }
+
+        // add the children to nodes
+        for (int i = 0; i < n; i++) {
+            if (!children.get(i).isEmpty()) {
+                TreeNode node = (TreeNode) trees[i];
+                for (int j : children.get(i)) {
+                    node.addChild(trees[j]);
+                }
+            }
+        }
+
+        return trees[0];
+    }
+
+    public void main() {
+        Tree root = new JavaVisitorPattern().solve();
+        SumInLeavesVisitor vis1 = new SumInLeavesVisitor();
+        ProductOfRedNodesVisitor vis2 = new ProductOfRedNodesVisitor();
+        FancyVisitor vis3 = new FancyVisitor();
+
+        root.accept(vis1);
+        root.accept(vis2);
+        root.accept(vis3);
+
+        int res1 = vis1.getResult();
+        int res2 = vis2.getResult();
+        int res3 = vis3.getResult();
+
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+    }
+
+    @Test
+    public void test1() throws Exception {
+        String input = "5\n4 7 2 5 12\n0 1 0 0 1\n1 2\n1 3\n3 4\n3 5";
+
+        System.setIn(new ByteArrayInputStream(input.getBytes("UTF-8")));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos, false, "UTF-8"));
+
+        main();
+
+        String output = "24\n40\n15";
+
+        Assert.assertEquals(output.trim(), baos.toString("UTF-8").trim());
+    }
 }
