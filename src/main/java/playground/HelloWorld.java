c895813415ca63f26eae53a95f7ab35bf52be55c
Adrian Francisco <adrian.francisco@gmail.com>
2017-04-27 10:21:42 -0400

    Update HelloWorld.java

--- src/main/java/playground/HelloWorld.java
+++ src/main/java/playground/HelloWorld.java
@@ -3,6 +3,7 @@ package playground;
 public class HelloWorld {
 
     public static void main(String... args) {
-        System.out.println("stupid jason");
+        System.out.println("change 1 from github");
+        System.out.println("change 2 from github");
     }
 }
