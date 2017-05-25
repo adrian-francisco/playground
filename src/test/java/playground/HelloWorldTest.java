84f13084c396d957523b21bc3d77f4eff7fd4f08
adrian.francisco <adrian.francisco@canada.ca>
2017-05-25 19:08:07 +0000

    added jacoco code coverage

---
+++ src/test/java/playground/HelloWorldTest.java
@@ -0,0 +1,34 @@
+package playground;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public final void test() {
+        HelloWorld.main();
+        assertTrue(true);
+    }
+}
