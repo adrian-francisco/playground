5d890d0c70a7fef355f72789c5486639963d55a0
adrian.francisco <adrian.francisco@canada.ca>
2017-06-09 13:39:29 +0000

    added a check snapshot to the accurev util

--- src/main/java/playground/CheckAccuRev.java
+++ src/main/java/playground/CheckAccuRev.java
@@ -7,12 +7,17 @@ package playground;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.ArrayUtils;
@@ -45,10 +50,21 @@ public class CheckAccuRev {
      */
     public static void main(String[] args) throws Exception {
 
-        //
-        // check active session
-        //
+        // uncomment the checks you wish to perform
+        // checkActiveSessions();
+        // checkWIP();
+        // checkStranded();
+        // checkSnapshot();
 
+        System.out.println("\nDone");
+    }
+
+    /**
+     * Check active sessions.
+     *
+     * @throws Exception the exception
+     */
+    private static void checkActiveSessions() throws Exception {
         boolean found = false;
         boolean first = true;
 
@@ -104,14 +120,18 @@ public class CheckAccuRev {
                 System.out.println("\nno active session found for user(s): " + Arrays.asList(DEVELOPERS));
             }
         }
+    }
 
-        //
-        // check active wip for each development stream
-        //
+    /**
+     * Check WIP.
+     *
+     * @throws Exception the exception
+     */
+    private static void checkWIP() throws Exception {
+        boolean found = false;
+        boolean first = true;
 
-        found = false;
-
-        for (String line : CommandExecutor.execute(ACCUREV + " show streams")) {
+        for (String line : CommandExecutor.execute(ACCUREV + " show -d -s MSC-DMS -ms streams")) {
 
             if (!line.startsWith("MSC")) {
                 continue;
@@ -168,8 +188,80 @@ public class CheckAccuRev {
                 System.out.println("\nno wip found for user(s): " + Arrays.asList(DEVELOPERS));
             }
         }
+    }
 
-        System.out.println("\ndone");
+    /**
+     * Check stranded.
+     */
+    private static void checkStranded() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * Check snapshot.
+     *
+     * @throws Exception the exception
+     */
+    private static void checkSnapshot() throws Exception {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        int maxDepotLength = 0;
+        int maxNameLength = 0;
+
+        // depot -> date -> [name, date, time]
+        Map<String, Map<String, String[]>> snapshots = new TreeMap<>();
+
+        for (String line : CommandExecutor.execute(ACCUREV + " show -s MSC-DMS -ms streams")) {
+
+            String[] split = line.split("\\s+");
+            String time = split[split.length - 1];
+
+            try {
+                format.parse(time);
+            }
+            catch (ParseException e) {
+                continue;
+            }
+
+            String name = split[0];
+            String depot = split[2];
+
+            if ("MSC-DMS-Core".equals(depot) && !name.startsWith("MSC-DMS-Core")) {
+                name = name.replace("MSC-DMS", "MSC-DMS-Core");
+            }
+
+            if ("MSC-DMS".equals(name)) {
+                name = "MSC-DMS-Core-2.0.0-Release";
+                depot = "MSC-DMS-Core";
+            }
+
+            String date = split[split.length - 2];
+
+            if (depot.length() > maxDepotLength) {
+                maxDepotLength = depot.length();
+            }
+
+            if (name.length() > maxNameLength) {
+                maxNameLength = name.length();
+            }
+
+            if (!snapshots.containsKey(depot)) {
+                snapshots.put(depot, new TreeMap<>());
+            }
+
+            snapshots.get(depot).put(date, new String[] {name, date, time});
+        }
+
+        String output = "%-" + maxDepotLength + "s %-" + maxNameLength + "s %s %s Canada/Eastern%n";
+
+        System.out.println("Generated on: " + new Date());
+        System.out.println();
+        for (Map.Entry<String, Map<String, String[]>> depotEntry : snapshots.entrySet()) {
+            for (Map.Entry<String, String[]> snapshotEntry : depotEntry.getValue().entrySet()) {
+                System.out.printf(output, depotEntry.getKey(), snapshotEntry.getValue()[0], snapshotEntry.getValue()[1],
+                        snapshotEntry.getValue()[2]);
+            }
+        }
     }
 
     /**
@@ -190,7 +282,7 @@ public class CheckAccuRev {
             BufferedReader stderr = null;
 
             try {
-                stdout = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("UTF-8")));
+                stdout = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
 
                 String line = null;
                 Collection<String> output = new ArrayList<>();
@@ -203,7 +295,7 @@ public class CheckAccuRev {
 
                 if (processResult != 0) {
                     stderr = new BufferedReader(
-                            new InputStreamReader(process.getErrorStream(), Charset.forName("UTF-8")));
+                            new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
 
                     while ((line = stderr.readLine()) != null) {
                         System.err.println(line);
